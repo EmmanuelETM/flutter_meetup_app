6f1dfa5cabbaeeb26cc4a0ed004a2218fa0967e2
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2024-11-12 19:30:54 -0400

    Finished the attendees functionality

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -22,19 +22,19 @@ class ApplicationState extends ChangeNotifier {
   int _attendees = 0;
   int get attendees => _attendees;
 
-Attending _attending = Attending.unknown;
-StreamSubscription<DocumentSnapshot>? _attendingSubscription;
-Attending get attending => _attending;
-set attending(Attending attending) {
-  final userDoc = FirebaseFirestore.instance
-      .collection('attendees')
-      .doc(FirebaseAuth.instance.currentUser!.uid);
-  if (attending == Attending.yes) {
-    userDoc.set(<String, dynamic>{'attending': true});
-  } else {
-    userDoc.set(<String, dynamic>{'attending': false});
+  Attending _attending = Attending.unknown;
+  StreamSubscription<DocumentSnapshot>? _attendingSubscription;
+  Attending get attending => _attending;
+  set attending(Attending attending) {
+    final userDoc = FirebaseFirestore.instance
+        .collection('attendees')
+        .doc(FirebaseAuth.instance.currentUser!.uid);
+    if (attending == Attending.yes) {
+      userDoc.set(<String, dynamic>{'attending': true});
+    } else {
+      userDoc.set(<String, dynamic>{'attending': false});
+    }
   }
-}
 
   StreamSubscription<QuerySnapshot>? _guestBookSubscription;
   List<GuestBookMessage> _guestBookMessages = [];
@@ -42,70 +42,70 @@ set attending(Attending attending) {
 
   Future<void> init() async {
     await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform);
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
 
     FirebaseUIAuth.configureProviders([
       EmailAuthProvider(),
     ]);
 
     FirebaseFirestore.instance
-        .collection('attendees')
-        .where('attending', isEqualTo: true)
-        .snapshots()
-        .listen((snapshot) {
-      _attendees = snapshot.docs.length;
-      notifyListeners();
-    });
-    
-    FirebaseAuth.instance.userChanges().listen((user) {
-      if (user != null) {
-        _loggedIn = true;
-        _emailVerified = user.emailVerified;
-        _guestBookSubscription = FirebaseFirestore.instance
+      .collection('attendees')
+      .where('attending', isEqualTo: true)
+      .snapshots()
+      .listen((snapshot) {
+        _attendees = snapshot.docs.length;
+        notifyListeners();
+      });
+
+      FirebaseAuth.instance.userChanges().listen((user) {
+        if (user != null) {
+          _loggedIn = true;
+          _emailVerified = user.emailVerified;
+          _guestBookSubscription = FirebaseFirestore.instance
             .collection('guestbook')
             .orderBy('timestamp', descending: true)
             .snapshots()
             .listen((snapshot) {
+              _guestBookMessages = [];
+              for (final document in snapshot.docs) {
+                _guestBookMessages.add(
+                  GuestBookMessage(
+                    name: document.data()['name'] as String, 
+                    message: document.data()['text'] as String,
+                  ),
+                );
+              }
+              notifyListeners();
+            });
+
+            _attendingSubscription = FirebaseFirestore.instance
+              .collection('attendees')
+              .doc(user.uid)
+              .snapshots()
+              .listen((snapshot) {
+                if (snapshot.data() != null) {
+                  if (snapshot.data()!['attending'] as bool) {
+                    _attending = Attending.yes;
+                  } else {
+                    _attending = Attending.no;
+                  } 
+                }else {
+                  _attending = Attending.unknown;
+                }
+                notifyListeners();
+              });
+        } else {
+          _loggedIn = false;
+          _emailVerified = false;
           _guestBookMessages = [];
-          for (final document in snapshot.docs) {
-            _guestBookMessages.add(
-              GuestBookMessage(
-                name: document.data()['name'] as String,
-                message: document.data()['text'] as String,
-              ),
-            );
-          }
-          notifyListeners();
-        });
-
-        _attendingSubscription = FirebaseFirestore.instance
-            .collection('attendees')
-            .doc(user.uid)
-            .snapshots()
-            .listen((snapshot) {
-          if (snapshot.data() != null) {
-            if (snapshot.data()!['attending'] as bool) {
-              _attending = Attending.yes;
-            } else {
-              _attending = Attending.no;
-            }
-          } else {
-            _attending = Attending.unknown;
-          }
-          notifyListeners();
-        });
-
-      } else {
-        _loggedIn = false;
-        _emailVerified = false;
-        _guestBookMessages = [];
-        _guestBookSubscription?.cancel();
-        _attendingSubscription?.cancel();
-      }
-      notifyListeners();
-    });
+          _guestBookSubscription?.cancel();
+          _attendingSubscription?.cancel();
+        }
+        notifyListeners();
+      });
   }
-  
+
   Future<DocumentReference> addMessageToGuestBook(String message) {
     if (!_loggedIn) {
       throw Exception('Must be logged in');
